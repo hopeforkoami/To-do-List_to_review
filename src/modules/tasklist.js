db9a70c7622903734699682eaa08f8f7b3c5272d
hopeforkoami <hopeforkoami@gmail.com>
2022-10-03 19:46:51 +0000

    Complete the project

---
+++ src/modules/tasklist.js
@@ -0,0 +1,58 @@
+import Task from './task.js';
+
+export default class TaskList {
+  constructor() {
+    this.localData = [];
+    const tempsData = JSON.parse(localStorage.getItem('taskList')) || [];
+    if (tempsData.length > 0) {
+      tempsData.forEach((element) => {
+        console.log(element);
+        this.localData.push(new Task(element.description, element.index, element.completed));
+      });
+    }
+  }
+
+  generateTodoIndex() {
+    return this.localData.length + 1;
+  }
+
+  refreshIndex() {
+    this.localData.forEach((task, index) => {
+      task.index = index + 1;
+    });
+    console.log('end of the refresh');
+    this.localStrore();
+  }
+
+  generateTaskList() {
+    let content = '';
+    this.localData.forEach((task) => {
+      content += task.renderLi();
+    });
+    return content;
+  }
+
+  addTask(description) {
+    this.localData.push(new Task(description, this.generateTodoIndex(), false));
+    this.refreshIndex();
+  }
+
+  removeItem(index) {
+    this.localData.splice(index, 1);
+    this.refreshIndex();
+  }
+
+  removeItem2(index) {
+    this.localData.splice(index, 1);
+  }
+
+  localStrore() {
+    console.log('local storage updated');
+    localStorage.setItem('taskList', JSON.stringify(this.localData));
+  }
+
+  clearCompletedTasks() {
+    this.localData = this.localData.filter((ontask) => ontask.completed === false);
+    this.refreshIndex();
+  }
+}
